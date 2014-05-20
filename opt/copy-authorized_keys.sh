f012e49cdf5224cbf59ab49874fcbc996337aa31
Michał Falszewski <michal.falszewski@laboratorium.ee>
2014-05-20 10:25:45 +0200

    delay for supervisor

--- opt/copy-authorized_keys.sh
+++ opt/copy-authorized_keys.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cp /srv/authorized_keys /home/$SSH_USER/.ssh/authorized_keys
-chown -R $SSH_USER:$SSH_USER /home/$SSH_USER
-chmod -R 600 /home/$SSH_USER/.ssh
+cp /srv/authorized_keys /home/$SSH_USER/.ssh/authorized_keys &&\
+chown -R $SSH_USER:$SSH_USER /home/$SSH_USER &&\
+chmod -R 600 /home/$SSH_USER/.ssh &&\
+sleep 3 &&\
 echo "Key for $SSH_USER added"
